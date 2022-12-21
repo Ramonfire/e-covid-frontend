bb9d4e1d653ac185520832cc69c8370810feb8fb
Ramonfire <omar.zaida@uir.ac.ma>
2022-12-21 09:59:33 +0100

    Mobile version
    added mobile version using capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Ramonfire.Ecovid',
+  appName: 'E-Covid',
+  webDir: 'dist/covid-public-page',
+  bundledWebRuntime: false
+};
+
+export default config;
